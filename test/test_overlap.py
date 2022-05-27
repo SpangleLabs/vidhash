225381f1ef64be85fa38d452dcde5914bae69757
Joshua Coales <joshua@coales.co.uk>
2022-05-27 19:32:18 +0100

    Renaming tests to match FrameCountMatch rename

--- test/test_overlap.py
+++ test/test_overlap.py
@@ -2,14 +2,14 @@ import vidhash
 from vidhash.match_options import FrameCountMatch, PercentageMatch, DurationMatch
 
 
-async def test_few_frames_absolute(butterfly_react_clip, butterfly_react_2sec_overlap_clip):
+async def test_few_frames_count(butterfly_react_clip, butterfly_react_2sec_overlap_clip):
     react_hash = await vidhash.hash_video(str(butterfly_react_clip))
     overlap_hash = await vidhash.hash_video(str(butterfly_react_2sec_overlap_clip))
     match = FrameCountMatch(count_overlap=5)
     assert match.check_match(react_hash, overlap_hash)
 
 
-async def test_many_frames_absolute(butterfly_react_clip, butterfly_react_2sec_overlap_clip):
+async def test_many_frames_count(butterfly_react_clip, butterfly_react_2sec_overlap_clip):
     react_hash = await vidhash.hash_video(str(butterfly_react_clip))
     overlap_hash = await vidhash.hash_video(str(butterfly_react_2sec_overlap_clip))
     match = FrameCountMatch(count_overlap=20)
